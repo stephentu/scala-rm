stephentu/scala-rm

commit 2676ac37cdc258ba992f6ececad79f915cbcb84b

Some more XML work as I prepare to deal with the almost unfixable XML equality situation (sure, I can be equal to four different classes and their four different hashcodes, why do you ask?)
git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/scala/trunk@18789 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

src/library/scala/xml/Text.scala
@@ -11,6 +11,10 @@
 
 package scala.xml
 
+object Text {
+  def apply(data: String) = new Text(data)
+  def unapply(other: Any) = other match { case x: Text => Some(x.data) ; case _ => None }
+}
 
 /** The class <code>Text</code> implements an XML node for text (PCDATA).
  *  It is used in both non-bound and bound XML representations.
@@ -19,14 +23,14 @@ package scala.xml
  *
  *  @param text the text contained in this node, may not be null.
  */
-case class Text(_data: String) extends Atom[String](_data)
+class Text(data: String) extends Atom[String](data)
 {
-  if (null == data)
+  if (data == null)
     throw new IllegalArgumentException("tried to construct Text with null")
 
+  /** XXX More hashCode flailing. */
   final override def equals(x: Any) = x match {
     case s:String  => s == data
-    case s:Text    => data == s.data
     case s:Atom[_] => data == s.data
     case _ => false
   }
@@ -39,5 +43,4 @@ case class Text(_data: String) extends Atom[String](_data)
    */
   override def buildString(sb: StringBuilder) =
     Utility.escape(data, sb)
-
 }

src/compiler/scala/tools/nsc/ast/parser/SymbolicXMLBuilder.scala
@@ -218,7 +218,7 @@ abstract class SymbolicXMLBuilder(p: Parsers#Parser, preserveWS: Boolean)
       
       Assign(Ident(_md), New(clazz, LL(attrArgs: _*)))
     }
-
+      
     def handlePrefixedAttribute(pre: String, key: String, value: Tree)  = mkAttributeTree(pre, key, value)
     def handleUnprefixedAttribute(key: String, value: Tree)             = mkAttributeTree(null, key, value)
 

src/dotnet-library/scala/xml/path/Expression.scala
@@ -1,1 +1,0 @@
-/* Expression.scala does not exist for the dotnet target */

src/library/scala/xml/MetaData.scala
@@ -29,28 +29,20 @@ object MetaData {
   @tailrec
   def concatenate(attribs: MetaData, new_tail: MetaData): MetaData =
     if (attribs eq Null) new_tail
-    else concatenate(attribs.next, attribs.copy(new_tail))
+    else concatenate(attribs.next, attribs copy new_tail)
 
   /**
    * returns normalized MetaData, with all duplicates removed and namespace prefixes resolved to
    *  namespace URIs via the given scope.
    */
-  def normalize(attribs: MetaData, scope: NamespaceBinding): MetaData = {
-    import collection.mutable.HashSet
-    def iterate(md: MetaData, normalized_attribs: MetaData, map: HashSet[String]): MetaData = {
-      if (md eq Null)
-        normalized_attribs
-      else {
-        val universal_key = getUniversalKey(md, scope)
-        if (map.contains(universal_key))
-          iterate(md.next, normalized_attribs, map)
-        else {
-          map += universal_key
-          iterate(md.next, md.copy(normalized_attribs), map)
-        } 
-      }
+  def normalize(attribs: MetaData, scope: NamespaceBinding): MetaData = {    
+    def iterate(md: MetaData, normalized_attribs: MetaData, set: Set[String]): MetaData = {
+      lazy val key = getUniversalKey(md, scope)
+      if (md eq Null) normalized_attribs
+      else if (set(key)) iterate(md.next, normalized_attribs, set)
+      else iterate(md.next, md copy normalized_attribs, set + key)
     }
-    iterate(attribs, Null, new HashSet[String])
+    iterate(attribs, Null, Set())
   }
  
   /**
@@ -156,19 +148,9 @@ abstract class MetaData extends Iterable[MetaData]
     case _            => false
   }
 
-  /** returns an iterator on attributes */
-  def iterator: Iterator[MetaData] = new Iterator[MetaData] {
-    var x: MetaData = MetaData.this
-    def hasNext = Null != x
-    def next = {
-      val y = x
-      x = x.next
-      y
-    }
-  }
-  override def size : Int = 1 + {
-    if (Null == next) 0 else next.size
-  }
+  /** Returns an iterator on attributes */
+  def iterator: Iterator[MetaData] = Iterator.iterate(this)(_.next) takeWhile (_ != Null)
+  override def size: Int = 1 + iterator.length
 
   /** shallow equals method */
   def equals1(that: MetaData): Boolean

src/library/scala/xml/PrettyPrinter.scala
@@ -25,7 +25,7 @@ import Utility.sbToString
  *  @param width the width to fit the output into
  *  @step  indentation
  */
-class PrettyPrinter( width:Int, step:Int ) {
+class PrettyPrinter(width: Int, step: Int) {
 
   class BrokenException() extends java.lang.Exception
 
@@ -174,7 +174,7 @@ class PrettyPrinter( width:Int, step:Int ) {
           val sb = new StringBuilder()
           Utility.toXML(node, pscope, sb, false)
           if (doPreserve(node)) sb.toString
-          else TextBuffer.fromString(sb.toString()).toText(0)._data
+          else TextBuffer.fromString(sb.toString()).toText(0).data
         }
         if (childrenAreLeaves(node) && fits(test)) {
           makeBox(ind, test)
@@ -300,8 +300,5 @@ class PrettyPrinter( width:Int, step:Int ) {
    *  @param sb   the string buffer to which to append to
    */
   def formatNodes(nodes: Seq[Node], pscope: NamespaceBinding, sb: StringBuilder): Unit =
-    for (n <- nodes.iterator) {
-      sb.append(format(n, pscope))
-    }
-
+    nodes foreach (n => sb append format(n, pscope))
 }

src/library/scala/xml/ProcInstr.scala
@@ -8,7 +8,6 @@
 
 // $Id$
 
-
 package scala.xml
 
 /** an XML node for processing instructions (PI)
@@ -17,50 +16,24 @@ package scala.xml
  * @param  target target name of this PI
  * @param  text   text contained in this node, may not contain "?>"
  */
-case class ProcInstr(target:String, proctext:String) extends SpecialNode
+case class ProcInstr(target: String, proctext: String) extends SpecialNode
 {  
-
   if (!Utility.isName(target))
     throw new IllegalArgumentException(target+" must be an XML Name")
-  if (text.indexOf("?>") != -1)
+  if (proctext contains "?>")
     throw new IllegalArgumentException(proctext+" may not contain \"?>\"")
-
+  if (target.toLowerCase == "xml")
+    throw new IllegalArgumentException(target+" is reserved")
+  
   final override def doCollectNamespaces = false
   final override def doTransform         = false
 
-  (target: Seq[Char]) match {
-    case Seq('X'|'x','M'|'m','L'|'l') =>
-      throw new IllegalArgumentException(target+" is reserved")
-    case _ =>
-  }
-  
-  /** structural equality */
-  override def equals(x: Any): Boolean = x match {
-    case ProcInstr(x, y) => x.equals(target) && y.equals(proctext)
-    case _ => false
-  }
-
-  /** the constant "#PI" */
-  final def label = "#PI"
-
-  /** hashcode for this PI */
-  override def hashCode() = target.hashCode() * 7 + proctext.hashCode()
-
-
+  final def label   = "#PI"  
   override def text = ""
 
   /** appends &quot;&lt;?&quot; target (&quot; &quot;+text)?+&quot;?&gt;&quot; 
    *  to this stringbuffer.
    */
-  override def buildString(sb: StringBuilder) = {
-    sb
-    .append("<?")
-    .append(target);
-    if (proctext.length() > 0) {
-      sb
-      .append(' ')
-      .append(proctext);
-    }
-    sb.append("?>")
-  }
+  override def buildString(sb: StringBuilder) =
+    sb append "<?%s%s?>".format(target, (if (proctext.isEmpty) "" else " " + proctext))
 }

src/library/scala/xml/TextBuffer.scala
@@ -11,9 +11,10 @@
 
 package scala.xml
 
+import Utility.isSpace
 
 object TextBuffer {
-  def fromString(str: String): TextBuffer = new TextBuffer().append(str)
+  def fromString(str: String): TextBuffer = new TextBuffer() append str
 }
 
 /** The class <code>TextBuffer</code> is for creating text nodes without
@@ -21,22 +22,20 @@ object TextBuffer {
  *  appended with the <code>append</code> method will be replaced by a single
  *  space character, and leading and trailing space will be removed completely.
  */
-class TextBuffer {
-
+class TextBuffer
+{
   val sb = new StringBuilder()
-  var ws = true
-
-  def appendSpace         = if(!ws) { ws = true;  sb.append(' ') } else {}
-  def appendChar(c: Char) =         { ws = false; sb.append( c ) }
 
   /** Appends this string to the text buffer, trimming whitespaces as needed.
    *
    *  @param  cs ...
    *  @return ...
    */
-  def append(cs: Seq[Char]): TextBuffer = {
-    for (c <- cs)
-      if (Utility.isSpace(c)) appendSpace else appendChar(c)
+  def append(cs: Seq[Char]): this.type = {
+    cs foreach { c => 
+      if (!isSpace(c)) sb append c
+      else if (sb.isEmpty || !isSpace(sb.last)) sb append ' '
+    }
     this
   }
 
@@ -44,17 +43,8 @@ class TextBuffer {
    *
    *  @return the text without whitespaces.
    */
-  def toText: Seq[Text] = {
-    var len = sb.length /* invariant */
-    if (len == 0) return Nil
-
-    if (Utility.isSpace(sb.charAt(len - 1))) {
-      len -= 1
-      sb.length = len
-    }
-    if (len == 0) return Nil
-
-    List(Text(sb.toString()))
+  def toText: Seq[Text] = sb.toString.trim match {
+    case "" => Nil
+    case s  => Seq(Text(s))
   }
-
 }

src/library/scala/xml/Unparsed.scala
@@ -22,10 +22,9 @@ class Unparsed(data: String) extends Atom[String](data)
   if (null == data)
     throw new IllegalArgumentException("tried to construct Unparsed with null")
 
+  /** XXX another hashCode fail */
   final override def equals(x: Any) = x match {
     case s:String   => s == data
-    case s:Text     => data == s.data
-    case s:Unparsed => data == s.data
     case s:Atom[_]  => data == s.data
     case _ => false
   }

commit 8db447d229b6e0f0484213fec2c6ad9d335af299

Fix for #2883, a regression introduced in r18789.  It was only a regression because the pattern matcher has extractor bugs which don't manifest for case classes.  Underlying bug remains.  No review.
git-svn-id: http://lampsvn.epfl.ch/svn-repos/scala/scala/trunk@20450 5e8d7ff9-d8ef-0310-90f0-a4852d11357a

src/library/scala/xml/Text.scala
@@ -13,16 +13,22 @@ package scala.xml
 
 import collection.mutable.StringBuilder
 
-object Text {
-  def apply(data: String) =
-    if (data != null) new Text(data)
-    else throw new IllegalArgumentException("tried to construct Text with null")
-    
-  def unapply(other: Any) = other match {
-    case x: Text  => Some(x.data)
-    case _        => None
-  }
-}
+// XXX This attempt to make Text not a case class revealed a bug in the pattern
+// matcher (see ticket #2883) so I've put the case back.  (It was/is desirable that
+// it not be a case class because it is using the antipattern of passing constructor
+// parameters to the superclass where they become vals, but since they will also be
+// vals in the subclass, it acquires an underscore to avoid a name clash.)
+//
+// object Text {
+//   def apply(data: String) =
+//     if (data != null) new Text(data)
+//     else throw new IllegalArgumentException("tried to construct Text with null")
+//     
+//   def unapply(other: Any): Option[String] = other match {
+//     case x: Text  => Some(x.data)
+//     case _        => None
+//   }
+// }
 
 /** The class <code>Text</code> implements an XML node for text (PCDATA).
  *  It is used in both non-bound and bound XML representations.
@@ -31,9 +37,9 @@ object Text {
  *
  *  @param text the text contained in this node, may not be null.
  */
-class Text(data: String) extends Atom[String](data)
+case class Text(_data: String) extends Atom[String](_data)
 {
-  if (data == null)
+  if (_data == null)
     throw new IllegalArgumentException("tried to construct Text with null")
 
   /** XXX More hashCode flailing. */