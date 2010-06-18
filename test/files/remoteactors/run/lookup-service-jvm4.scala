4e5a94d3d74c6433fafe8a00dffab8ae4b057926
Stephen Tu <stephen_tu@berkeley.edu>
2010-06-18 15:15:14 -0700

    fixed improper test files

--- test/files/remoteactors/run/lookup-service-jvm4.scala
+++ test/files/remoteactors/run/lookup-service-jvm4.scala
@@ -1,1 +1,26 @@
-Starting stop client...
+import scala.actors._
+import Actor._
+import remote._
+import RemoteActor._
+
+import scala.tools.partest.FileSync._
+
+case class StopService()
+case class GetRequest(request: String)
+case class GetResponse(request: GetRequest, response: Option[String])
+
+object StopClient extends Actor {
+  def act() {
+    val service = select(Node("127.0.0.1", 9100), 'lookupService)
+    service ! StopService()
+  }
+}
+
+object Test {
+  def main(args: Array[String]) {
+    println("Starting stop client...")
+    waitForFiles(Array(0,1,2))
+    StopClient.start
+  }
+}
+
