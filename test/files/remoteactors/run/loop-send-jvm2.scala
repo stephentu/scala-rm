6345d233d3a256409d5e99cbeb2fd625ec49dbd1
Stephen Tu <stephen_tu@berkeley.edu>
2010-06-28 00:34:56 -0700

    add loop sending tests

---
+++ test/files/remoteactors/run/loop-send-jvm2.scala
@@ -0,0 +1,28 @@
+import scala.actors._
+import Actor._
+import remote._
+import RemoteActor._
+
+import scala.tools.partest.FileSync._
+
+object Test2 {
+  def main(args: Array[String]) {
+    Debug.level = 0
+    //waitFor(0)
+    def msg = "test2 -> test1"
+    actor {
+      val test1 = select(Node("127.0.0.1", 9112), 'test1)
+      test1 ! msg
+      var i = 1
+      loopWhile(i <= 10000) {
+        react {
+          case "test1 -> test2" =>
+            i += 1
+            if ((i % 1000) == 0) println("On i = " + i)
+            sender ! msg
+        }
+      }
+      println("Test2 is done!")
+    }
+  }
+}
---
+++ test/files/remoteactors/run/loop-send-nio-jvm1.scala
@@ -0,0 +1,27 @@
+import scala.actors._
+import Actor._
+import remote._
+import RemoteActor._
+
+import scala.tools.partest.FileSync._
+
+object Test1 {
+  def main(args: Array[String]) {
+    Debug.level = 0
+    actor {
+      alive(9111, serviceFactory = NioServiceFactory)
+      register('test1, self)
+      //writeFlag()
+      var i = 1
+      loopWhile(i <= 10000) {
+        react {
+          case "test2 -> test1" =>
+            i += 1
+            if ((i % 1000) == 0) println("On i = " + i)
+            sender ! "test1 -> test2"
+        }
+      }
+      println("Test1 is done!")
+    }
+  }
+}
